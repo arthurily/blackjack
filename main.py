a959193dc64bf6308b27011456a72838315199b2
Arthur Ilyasov <arthuroilyasov@gmail.com>
2025-07-04 03:34:41 -0400

    created a dealCard function and calculateSum

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', ]
+
+playerHand = []
+dealerHand = []
+
+playerSum = 0
+dealerSum = 0
+
+def dealCard():
+    card1 = random.choice(deck)
+    playerHand.append(card1)
+    deck.remove(card1)
+
+    card2 = random.choice(deck)
+    dealerHand.append(card2)
+    deck.remove(card2)
+
+    card3 = random.choice(deck)
+    playerHand.append(card3)
+    deck.remove(card3)
+
+    card4 = random.choice(deck)
+    dealerHand.append(card4)
+    deck.remove(card4)
+
+    playerSum = calculateSum(playerHand)
+    dealerSum = calculateSum(dealerHand)
+
+    print(f"Player's hand: {playerHand} your total is: {playerSum}" )
+    print(f"Dealer's hand: {dealerHand[0]} and a hidden card")
+
+def calculateSum(hand):
+    total = 0
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            total += 11
+            aces += 1
+        else:
+            total += card
+
+    while total > 21 and aces:
+        total -= 10
+        aces -= 1
+
+    return total
+
+dealCard()
